1c2f895c1046044f78aadffab6b307ed9a318d28
Dustin Pho <dpho@vt.edu>
2014-11-14 16:10:31 -0500

    Added JUnit and HamCrest to build path, added java docs for Image and Pixel classes

--- simple-ocr/src/Image.java
+++ simple-ocr/src/Image.java
@@ -4,44 +4,80 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * Image class that contains a 2-Dimensional array of Pixel objects.
+ * 
+ * @author Dustin
+ * 
+ */
 public class Image {
 
 	private Pixel[][] imageArray;
 	private int height;
 	private int width;
 
+	/**
+	 * Creates an Image object.
+	 * 
+	 * @param imagePath
+	 *            path to our image
+	 */
 	public Image(String imagePath) {
 		File image = new File(imagePath);
 		imageToArray(image);
 	}
 
+	/**
+	 * Initializes our height, width and imageArray
+	 * 
+	 * @param imageFile
+	 *            Our image.
+	 */
 	private void imageToArray(File imageFile) {
+		// Creating a BufferedImage and initializing height, width
 		BufferedImage bufferedImage;
 		try {
 			bufferedImage = ImageIO.read(imageFile);
 			this.height = bufferedImage.getHeight();
 			this.width = bufferedImage.getWidth();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			System.out.println("Error reading image file.");
 			return;
 		}
 
+		// Initializing our imageArray
 		for (int i = 0; i < bufferedImage.getHeight(); i++) {
 			for (int j = 0; j < bufferedImage.getWidth(); j++) {
 
-				int[] rgbValues = bufferedImage.getRGB(i, j,
-						bufferedImage.getWidth(), bufferedImage.getHeight(),
-						null, 0, bufferedImage.getWidth());
+				int rgbaValues = bufferedImage.getRGB(i, j);
 
-				Pixel pixel = new Pixel(i, j, (rgbValues[100] >> 16) & 0xFF,
-						(rgbValues[100] >> 8) & 0xFF,
-						(rgbValues[100] >> 0) & 0xFF,
-						(rgbValues[100] >> 24) & 0xFF);
+				Pixel pixel = new Pixel(i, j, (rgbaValues >> 16) & 0xFF,
+						(rgbaValues >> 8) & 0xFF, (rgbaValues >> 0) & 0xFF,
+						(rgbaValues >> 24) & 0xFF);
 
 				imageArray[i][j] = pixel;
 			}
 		}
 
 	}
+
+	/**
+	 * Gets height of image
+	 * 
+	 * @return height in pixels
+	 */
+	public int getHeight() {
+		return height;
+	}
+
+	/**
+	 * Gets width of image
+	 * 
+	 * @return width in pixels
+	 */
+	public int getWidth() {
+		return width;
+	}
+
 }
--- simple-ocr/src/Pixel.java
+++ simple-ocr/src/Pixel.java
@@ -1,12 +1,58 @@
+/**
+ * Simple Pixel object that has RED/GREEN/BLUE/ALPHA (RGBA) values and knowledge
+ * of its position in an image.
+ * 
+ * @author Dustin Pho
+ * 
+ */
 public class Pixel {
 
+	/**
+	 * x-coordinate
+	 */
 	public final int X;
+
+	/**
+	 * y-coordinate
+	 */
 	public final int Y;
+
+	/**
+	 * red value of RGBA
+	 */
 	public int RED;
+
+	/**
+	 * green value of RGBA
+	 */
 	public int GREEN;
+
+	/**
+	 * blue value of RGBA
+	 */
 	public int BLUE;
+
+	/**
+	 * alpha value of RGBA
+	 */
 	public int ALPHA;
 
+	/**
+	 * Constructs a pixel object.
+	 * 
+	 * @param x
+	 *            x-coordinate of Pixel
+	 * @param y
+	 *            y-coordinate of Pixel
+	 * @param red
+	 *            red RGBA value
+	 * @param green
+	 *            green RGBA value
+	 * @param blue
+	 *            blue RGBA value
+	 * @param alpha
+	 *            alpha RGBA value
+	 */
 	public Pixel(int x, int y, int red, int green, int blue, int alpha) {
 		this.X = x;
 		this.Y = y;
